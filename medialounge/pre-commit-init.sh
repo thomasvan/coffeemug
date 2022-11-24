0063b694df608336661eefd3692a9dd8de55a3b6
thongvan <thongvan@live.com>
2022-11-24 15:28:35 +0700

    add the ML pre-commit initialization

---
+++ medialounge/pre-commit-init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+<<\HOOKS tee ./.git/hooks/pre-commit > /dev/null
+#!/usr/bin/env bash
+
+echo "... pre-commit hook start"
+
+echo "... checking author's email"
+email_domain=${GIT_AUTHOR_EMAIL#*@}
+if [[ $email_domain != 'medialounge.co.uk' ]]; then
+    echo "your domain of email must be medialounge.co.uk"
+    exit 1
+fi
+echo "author's email validation: OK"
+
+HOOKS
+
+chmod +x ./.git/hooks/pre-commit 
